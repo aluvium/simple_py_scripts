098f1a845ee941054b9f6a9d657573570d0d2850
Aluvium <aluvium@protinmail.com>
2021-11-06 18:49:55 +0100

    Your dead calculator, changes and improvement.

--- dead_calc.py
+++ dead_calc.py
@@ -9,14 +9,15 @@ dead_art.d_a()
 age=eval(input('Enter your age: '))
 age_d=age*356
 print(f'You already live approximately {age_d} days.')
+
 dead=eval(input("\n\nAverage Life Expectancy for people is 75 years. \nWhat do You think? How many years are you going to live: "))
 dead_d=dead*356
 print("\n\nThis is rest of your life.")
 time.sleep(2)
 
-for each in range(dead_d):
+for each in range(dead_d-age_d+1):
     print(f'This is your {each} day')
-    time.sleep(0.00001)
+    #time.sleep(0.0000001)
 
-print ('\nR. I. P.')
-print(f'\nYou have still {len(range(age_d,dead_d))} days of living. Spend it wisely.\nHappy living :).')
+print ('\n + + + R. I. P. + + +')
+print(f'\nYou have still {each} days of living.\nSpend it wisely. Happy living!! :).')
